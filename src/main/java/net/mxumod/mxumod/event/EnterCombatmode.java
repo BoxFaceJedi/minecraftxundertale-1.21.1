57e3b53474302aa9deb57c6a2c1366c84bad376d
BoxFaceJedi <jerryyujeng.tseng@gmail.com>
2024-12-02 18:50:37 +0800

    added cancelling for block interaction and entity interaction of player during combat mode

--- src/main/java/net/mxumod/mxumod/event/EnterCombatmode.java
+++ src/main/java/net/mxumod/mxumod/event/EnterCombatmode.java
@@ -1,10 +1,13 @@
 package net.mxumod.mxumod.event;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.network.chat.Component;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.entity.player.AttackEntityEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.mxumod.mxumod.MxuMod;
 
+@Mod.EventBusSubscriber(modid = MxuMod.MOD_ID, value = Dist.CLIENT)
 public class EnterCombatmode {
     public static boolean isCombatmode() {
         return combatmode;
@@ -17,12 +20,19 @@ public class EnterCombatmode {
     private static boolean combatmode;
 
     @SubscribeEvent
-    public  static void inCombatmode(PlayerInteractEvent.LeftClickBlock event) {
-        Minecraft.getInstance().player.sendSystemMessage(Component.literal("interaction found"));
-        if (combatmode) {
-            Minecraft.getInstance().player.sendSystemMessage(Component.literal("cancelling event"));
-            event.setCanceled(true);
-            Minecraft.getInstance().player.sendSystemMessage(Component.literal("event canceled"));
-        }
+    public  static void onLeftClickBlock (PlayerInteractEvent.LeftClickBlock event) {
+        event.setCanceled(combatmode);
+    }
+    @SubscribeEvent
+    public  static void onRightClickBlock (PlayerInteractEvent.RightClickBlock event) {
+        event.setCanceled(combatmode);
+    }
+    @SubscribeEvent
+    public  static void onAttackEntity (AttackEntityEvent event) {
+        event.setCanceled(combatmode);
+    }
+    @SubscribeEvent
+    public  static void onRightClickEntityInteract (PlayerInteractEvent.EntityInteract event) {
+        event.setCanceled(combatmode);
     }
 }
