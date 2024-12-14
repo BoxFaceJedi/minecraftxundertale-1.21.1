9c7ed76c48f5ec654b63d3697cf414cd3f12af8f
BoxFaceJedi <jerryyujeng.tseng@gmail.com>
2024-12-14 19:51:57 +0800

    now pressing left click fires the arrow

--- src/main/java/net/mxumod/mxumod/networking/packet/MxuTestC2SPacket.java
+++ src/main/java/net/mxumod/mxumod/networking/packet/MxuTestC2SPacket.java
@@ -3,8 +3,10 @@ package net.mxumod.mxumod.networking.packet;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.MobSpawnType;
+import net.minecraft.world.entity.projectile.Arrow;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.event.network.CustomPayloadEvent;
 
 public class MxuTestC2SPacket {
@@ -23,9 +25,19 @@ public class MxuTestC2SPacket {
     public void handle(CustomPayloadEvent.Context context) {
         context.enqueueWork(() -> {
             ServerPlayer player = context.getSender();
+
             assert player != null;
             ServerLevel level = player.serverLevel().getLevel();
-            EntityType.ARROW.spawn(level,null, null, player.getOnPos(), MobSpawnType.COMMAND, true, false);
+
+            Arrow arrow = new Arrow(level,player.getX(), player.getEyeY(), player.getZ(), new ItemStack(Items.ARROW, 1), null);
+
+            Vec3 lookVec = player.getLookAngle();
+
+            arrow.shoot(lookVec.x, lookVec.y, lookVec.z, 2.0f, 0.0f);
+
+            arrow.setOwner(player);
+
+            level.addFreshEntity(arrow);
         });
     }
 }
