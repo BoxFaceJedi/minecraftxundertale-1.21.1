e131b017e0a5a83013c24572980e1b86eac7163d
BoxFaceJedi <jerryyujeng.tseng@gmail.com>
2024-12-19 17:08:29 +0800

    added CameraLock wip

--- src/main/java/net/mxumod/mxumod/skill/CameraLock.java
+++ src/main/java/net/mxumod/mxumod/skill/CameraLock.java
@@ -1,15 +1,23 @@
 package net.mxumod.mxumod.skill;
 
+import net.minecraft.commands.arguments.EntityAnchorArgument;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.phys.EntityHitResult;
+import net.minecraft.world.phys.HitResult;
 import net.minecraft.world.phys.Vec3;
 
 public class CameraLock {
-    public static void cameraLockOn(Player player) {
-        player.lookAt();
+    public static void cameraLockOn(Player player) {player.lookAt(EntityAnchorArgument.Anchor.EYES, entityInView(player, 20));
     }
 
     private static Vec3 entityInView(Player player, double distance) {
-        player.pick(distance, 0, true );
+        HitResult hitResult = player.pick(distance, 0, true );
+
+        if (hitResult instanceof EntityHitResult entityHitResult) {
+            return entityHitResult.getEntity().position();
+        }else {
+            return null;
+        }
     }
 }
