cebd4e67d8d67b2d0d4dcb2acbd08ff221dcce56
BoxFaceJedi <jerryyyujeng.tseng@gmail.com>
2024-12-26 16:12:09 +0800

    disabled q and e in combat mode

--- src/main/java/net/mxumod/mxumod/event/EnterCombatmode.java
+++ src/main/java/net/mxumod/mxumod/event/EnterCombatmode.java
@@ -1,7 +1,15 @@
 package net.mxumod.mxumod.event;
 
+import net.minecraft.client.gui.screens.inventory.InventoryScreen;
+import net.minecraft.client.player.Input;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.inventory.InventoryMenu;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.ScreenEvent;
+import net.minecraftforge.event.entity.item.ItemTossEvent;
 import net.minecraftforge.event.entity.player.AttackEntityEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -20,17 +28,29 @@ public class EnterCombatmode {
     private static boolean combatmode;
 
     @SubscribeEvent
-    public  static void onLeftClickBlock (PlayerInteractEvent.LeftClickBlock event) {
+    public static void onLeftClickBlock (PlayerInteractEvent.LeftClickBlock event) {
         event.setCanceled(combatmode);
     }
     @SubscribeEvent
-    public  static void onRightClickBlock (PlayerInteractEvent.RightClickBlock event) {
+    public static void onRightClickBlock (PlayerInteractEvent.RightClickBlock event) {
         event.setCanceled(combatmode);
     }
     @SubscribeEvent
-    public  static void onAttackEntity (AttackEntityEvent event) {
+    public static void onAttackEntity (AttackEntityEvent event) {
         event.setCanceled(combatmode);
     }
     @SubscribeEvent
-    public  static void onRightClickEntityInteract (PlayerInteractEvent.EntityInteract event) {event.setCanceled(combatmode);}
+    public static void onRightClickEntityInteract (PlayerInteractEvent.EntityInteract event) {event.setCanceled(combatmode);}
+    @SubscribeEvent
+    public static void onOpenInventory (ScreenEvent event) {
+        if (event.getScreen() instanceof InventoryScreen && combatmode) {
+            event.setCanceled(true);
+        }
+    }
+    @SubscribeEvent
+    public static void disabledDropItem(ItemTossEvent event) {
+        event.getPlayer().addItem(event.getEntity().getItem());
+        event.getEntity().remove(Entity.RemovalReason.DISCARDED);
+    }
+
 }
