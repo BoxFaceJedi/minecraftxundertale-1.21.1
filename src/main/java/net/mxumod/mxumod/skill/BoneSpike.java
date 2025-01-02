7e9c5b305e4931e606f1cd6bd3106b852f793d35
BoxFaceJedi <jerryyujeng.tseng@gmail.com>
2025-01-02 20:58:21 +0800

    added some weird knock back

--- src/main/java/net/mxumod/mxumod/skill/BoneSpike.java
+++ src/main/java/net/mxumod/mxumod/skill/BoneSpike.java
@@ -4,14 +4,20 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.EvokerFangs;
+import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.event.entity.living.LivingAttackEvent;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+
+import java.lang.reflect.Array;
+import java.util.List;
 
 @Mod.EventBusSubscriber()
 public class BoneSpike {
@@ -19,11 +25,16 @@ public class BoneSpike {
     public static void boneSpikeAttack(ServerPlayer player) {
         ServerLevel level = player.serverLevel().getLevel();
         Vec3 posInFront = getPositionInFrontOfPlayer(player, 1);
+        List<LivingEntity> entityInFront = level.getEntitiesOfClass(LivingEntity.class, AABB.ofSize(posInFront, 1, 1, 1));
 
         EvokerFangs bone_spike = new EvokerFangs(EntityType.EVOKER_FANGS, level);
         bone_spike.setPos(posInFront.x, posInFront.y, posInFront.z);
         bone_spike.setOwner(player);
         level.addFreshEntity(bone_spike);
+
+        for (LivingEntity entity : entityInFront) {
+            entity.setDeltaMovement(entity.getDeltaMovement().add(0.0, 1.5, 0.0));
+        }
     }
 
     public static Vec3 getPositionInFrontOfPlayer(ServerPlayer player, double distance) {
@@ -40,8 +51,10 @@ public class BoneSpike {
 
     @SubscribeEvent
     public static void boneSpikeHit(LivingHurtEvent event) {
-        if (event.getSource().getEntity() instanceof EvokerFangs) {
-            event.getEntity().setDeltaMovement(0, 10, 0);
+        if (event.getSource().getDirectEntity() instanceof EvokerFangs fangs) {
+            if (fangs.getOwner() instanceof Player) {
+                event.getEntity().setDeltaMovement(0.0, 10.0, 0.0);
+            }
         }
     }
 }
