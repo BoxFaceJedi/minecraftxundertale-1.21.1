77b6ba22dde5b0b3321eef51a08f2ae6388758ce
BoxFaceJedi <jerryyujeng.tseng@gmail.com>
2025-01-03 11:55:23 +0800

    dodge is now weirder than ever before

--- src/main/java/net/mxumod/mxumod/skill/Dodge.java
+++ src/main/java/net/mxumod/mxumod/skill/Dodge.java
@@ -1,27 +1,14 @@
 package net.mxumod.mxumod.skill;
 
-import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.event.entity.living.LivingAttackEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.mxumod.mxumod.MxuMod;
 
-import java.util.concurrent.atomic.AtomicBoolean;
 @Mod.EventBusSubscriber(modid = MxuMod.MOD_ID, value = Dist.CLIENT)
 public class Dodge {
-    private static final AtomicBoolean isMoving = new AtomicBoolean(false);
 
     public static void dodge(Player player, double speed) {
         player.setDeltaMovement(player.getDeltaMovement().normalize().multiply(speed, 1, speed));
-        player.sendSystemMessage(Component.literal(String.valueOf(player.getDeltaMovement().length())));
-    }
-    @SubscribeEvent
-    public static void onLivingEntityAttack(LivingAttackEvent event) {
-        if (event.getEntity() instanceof Player player && player.getDeltaMovement().length() > 1.0) {
-            event.setCanceled(true);
-        }
     }
 }
