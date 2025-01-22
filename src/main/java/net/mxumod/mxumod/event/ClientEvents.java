33f22093a11768d5233486dab267589cf4fa00dd
fatalcode1572 <enoch404707@gmail.com>
2025-01-22 11:13:42 +0800

    Added Stamina.

--- src/main/java/net/mxumod/mxumod/event/ClientEvents.java
+++ src/main/java/net/mxumod/mxumod/event/ClientEvents.java
@@ -56,7 +56,7 @@ public class ClientEvents {
                     }
                 }else {
                     if (Keybinding.SETTINGS.consumeClick()) {
-                        OptionInstance
+
                     }
                 }
             }
