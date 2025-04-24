ce6a52c92efaa0d98346cfbe032230e9f6340363
Hello_World10124 <s210013@mail.hwsh.tc.edu.tw>
2025-04-24 14:32:16 +0800

    Test

--- src/main/java/net/team/mxumod/minecraftxundertale/skill/special/Telekinesis.java
+++ src/main/java/net/team/mxumod/minecraftxundertale/skill/special/Telekinesis.java
@@ -3,8 +3,10 @@ package net.team.mxumod.minecraftxundertale.skill.special;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.LivingEntity;
 import net.team.mxumod.minecraftxundertale.skill.Skill;
+import org.lwjgl.opengl.INTELBlackholeRender;
 
 import java.util.List;
+import java.util.Map;
 
 public class Telekinesis extends Skill {
     public Telekinesis()  {
@@ -13,10 +15,8 @@ public class Telekinesis extends Skill {
 
     @Override
     public void executeSkill(ServerPlayer player, Object data) {
-        if (data instanceof List<?> list) {
-            if (list.get(0) instanceof LivingEntity target && list.get(1) instanceof String key) {
+        if (data instanceof List<String> list) {
 
-            }
         }
     }
 }
